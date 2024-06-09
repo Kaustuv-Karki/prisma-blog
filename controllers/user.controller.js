95994050a26ebb9bd3a2a1bc1caf15b74eb4982b
Kaustuv Karki <karkikaustuv@gmail.com>
2024-06-09 10:57:14 +0545

    Prisma post resquest

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -26,3 +26,34 @@ export const createUser = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 };
+
+export const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+
+  const findUser = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!findUser) {
+    return res.status(400).json({ error: "User not found" });
+  }
+
+  try {
+    const user = await prisma.user.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        name: name,
+        email: email,
+        password: password,
+      },
+    });
+    res.status(200).json({ user, message: "User updated successfully" });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createUser } from "../controllers/user.controller.js";
+import { createUser, updateUser } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/register", createUser);
+router.put("/update/:id", updateUser);
 
 export default router;
