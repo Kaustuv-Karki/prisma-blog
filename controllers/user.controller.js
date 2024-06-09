bbdb3c8cefc1b1e1919d5ad81f90791a701a0619
Kaustuv Karki <karkikaustuv@gmail.com>
2024-06-09 11:09:28 +0545

    Prisma crud operations

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -57,3 +57,53 @@ export const updateUser = async (req, res) => {
     console.log(error);
   }
 };
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getUsersById = async (req, res) => {
+  const { id } = req.params;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({ error: "User not found" });
+  }
+
+  return res.status(200).json(user);
+};
+
+export const deleteUser = async (req, res) => {
+  const { id } = req.params;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: parseInt(id),
+    },
+  });
+
+  if (!user) {
+    return res.status(400).json({ error: "User not found" });
+  }
+
+  try {
+    await prisma.user.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    return res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { createUser, updateUser } from "../controllers/user.controller.js";
+import {
+  createUser,
+  updateUser,
+  getAllUsers,
+  getUsersById,
+  deleteUser,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
 router.post("/register", createUser);
 router.put("/update/:id", updateUser);
+router.get("/get", getAllUsers);
+router.get("/get/:id", getUsersById);
+router.delete("/delete/:id", deleteUser);
 
 export default router;
